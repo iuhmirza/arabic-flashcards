9a68be4b28ebf758aa794a2330090539078627fc
Ibtisam Mirza <iuhmirza@gmail.com>
2022-10-20 16:13:36 +0100

    Add random flash card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-const App = () => {
-  
+import { useState } from "react"
+
+const CurrentFlashcard = ({flashcard}) => {
   return (
     <div>
-      <p>
-        "Hello world!"
-      </p>
+      <p>{flashcard.english}</p>
+      <p>{flashcard.arabic}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [currentCardIndex, setCurrentCardIndex] = useState(0)
+  const flashcards = [
+    {
+      english: "He did",
+      arabic: "فَعَلَ"
+    },
+    {
+      english: "He was",
+      arabic: "كَانَ"
+    }
+  ]
+
+  return (
+    <div>
+      <CurrentFlashcard  flashcard={flashcards[currentCardIndex]}/>
+      <button onClick={() => setCurrentCardIndex(Math.floor(Math.random()*flashcards.length))}>Random Card</button>
     </div>
   )
 }
