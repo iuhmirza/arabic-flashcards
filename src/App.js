a75f26a0e2daa79d696bf2616afcd79c33b20e32
Ibtisam Mirza <iuhmirza@gmail.com>
2022-11-17 16:33:17 +0000

    Add arabic tutor vocab list no.1

--- src/App.js
+++ src/App.js
@@ -23,7 +23,132 @@ const App = () => {
     {
       english: "This is",
       arabic: "هٰذَا"
-    }
+    }, 
+    {
+      english: "Man",
+      arabic: "اِنْسَانٌ"
+    },
+    {
+      english: "House",
+      arabic: "بَيْتٌ"
+    },
+    {
+      english: "Dates",
+      arabic: "تَمْرٌ"
+    },
+    {
+      english: "Fruit",
+      arabic: "ثَمْرٌ"
+    },
+    {
+      english: "Ignorant",
+      arabic: "جَاهِل"
+    },
+    {
+      english: "Learned",
+      arabic: "عَالِمٌ"
+    },
+    {
+      english: "Good, Beautiful",
+      arabic: "حَسَنٌ"
+    },
+    {
+      english: "Bread",
+      arabic: "خُبْزٌ"
+    },
+    {
+      english: "Lesson",
+      arabic: "دَرْسٌ"
+    },
+    {
+      english: "Sin",
+      arabic: "ذَنْبٌ"
+    },
+    {
+      english: "Messenger",
+      arabic: "رَسُوْلٌ"
+    },
+    {
+      english: "Zakah",
+      arabic: "زَكَاةٌ"
+    },
+    {
+      english: "Easy",
+      arabic: "سَهْلٌ"
+    },
+    {
+      english: "Thing",
+      arabic: "شَيْئٌ"
+    },
+    {
+      english: "Prayer",
+      arabic: "صَلَاةٌ"
+    },
+    {
+      english: "Light",
+      arabic: "ضَوْءٌ"
+    },
+    {
+      english: "Good, Clean",
+      arabic: "طَيِّبٌ"
+    },
+    {
+      english: "Oppressor",
+      arabic: "ظَالِمٌ"
+    },
+    {
+      english: "Just",
+      arabic: "عَادِلٌ"
+    },
+    {
+      english: "One who forgives",
+      arabic: "غَفُوْرٌ"
+    },
+    {
+      english: "Transgressor",
+      arabic: "فَاسِقٌ"
+    },
+    {
+      english: "Ugly",
+      arabic: "قَبِيْحٌ"
+    },
+    {
+      english: "Generous",
+      arabic: "كَرِيْمٌ"
+    },
+    {
+      english: "Milk",
+      arabic: "لَبَنٌ"
+    },
+    {
+      english: "Water",
+      arabic: "مَاءٌ"
+    },
+    {
+      english: "This is",
+      arabic: "نَهَارٌ"
+    },
+    {
+      english: "Boy",
+      arabic: "وَلَدٌ"
+    },
+    {
+      english: "Cat",
+      arabic: "هِرٌّ"
+    },
+    {
+      english: "Day",
+      arabic: "يَوْمٌ"
+    },
+    {
+      english: "And",
+      arabic: "وَ"
+    },
+    {
+      english: "Or",
+      arabic: "أوْ"
+    },
+    
   ]
 
   const randomFlashcard = () => {
